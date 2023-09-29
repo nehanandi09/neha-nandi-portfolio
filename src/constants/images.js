4ee63b732f924d3f2f8ddfea2bca20aa542070e4
Neha Nandi <nandineha09@gmail.com>
2023-09-29 20:49:53 +0100

    Replace resume

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,6 @@
 import heroImage from "../assets/neha_dp_2.jpeg";
 import logo from "../assets/neha_logo.png";
-import Resume from "../assets/Neha_Resume.pdf";
+import Resume from "../assets/RESUME-Neha_Nandi.pdf";
 
 export default {
   heroImage,
